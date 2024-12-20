1a2dcb954e66902b1a522cdd68ffdc053a970e9e
RasulSz22 <rasulsabziyev@gmail.com>
2024-12-20 12:17:12 +0400

    SCs

--- Shop.Core/Entities/Models/Category.cs
+++ Shop.Core/Entities/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using Shop.Core.Entities.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Shop.Core.Entities.Models
 {
     public class Category : BaseEntity
     {
+        [ForeignKey("ParentId")]
         public string? Name { get; set; }
         public int ParentId {  get; set; }
         public Category Parent { get; set; }
