d48bedda0ca3a0fe74519d0aae0b28f9a6ef9543
Rasul <Rasul@MOODGG>
2024-12-24 11:31:54 +0400

    SC

---
+++ Shop.Businness/Registration/ServiceLayerServiceRegister.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shop.Businness.Services.Implementations;
+using Shop.Businness.Services.Interfaces;
+using Shop.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Businness.Registration
+{
+    //public static void ServiceLayerServiceRegister(this IServiceCollection services)
+    //{
+    //    services.AddScoped<IAddressService, AddressService>();
+    //}
+}
