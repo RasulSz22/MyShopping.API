89070bbc61823052f0652b73e6a5fb16490c7d49
Rasul <Rasul@MOODGG>
2024-12-25 11:11:26 +0400

    Update and Reset DTOs Completed Successfully

--- Shop.DTO/UserDTO/ResetPassword.cs
+++ Shop.DTO/UserDTO/ResetPassword.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace Shop.DTO.UserDTO
 {
     public record ResetPassword
     {
+        [Required(ErrorMessage = "Email must be entered!")]
+        [StringLength(40, MinimumLength = 10, ErrorMessage = "Email must be between 10 and 40 characters")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Wrong Email!")]
+        public string Email { get; set; } = null!;
+        public string Token { get; set; } = null!;
+        [Required(ErrorMessage = "New Password must be entered!")]
+        [StringLength(25, MinimumLength = 8, ErrorMessage = "New Password must be between 8 and 25 characters")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
     }
 }
--- Shop.DTO/UserDTO/UpdateDTO.cs
+++ Shop.DTO/UserDTO/UpdateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace Shop.DTO.UserDTO
 {
     public record UpdateDTO
     {
+        [Required(ErrorMessage = "Username must be entered!")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 25 characters")]
+        public string UserName { get; set; } = null!;
+        [DataType(DataType.Password)]
+        [StringLength(25, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 25 characters")]
+        public string? OldPassword { get; set; }
+        [DataType(DataType.Password)]
+        [StringLength(25, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 25 characters")]
+        public string? NewPassword { get; set; }
     }
 }
