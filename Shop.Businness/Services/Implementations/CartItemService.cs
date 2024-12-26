54f33757d10af46775253e814891bb0c449c83d5
Rasul <Rasul@MOODGG>
2024-12-26 12:44:40 +0400

    Nese eledim

--- Shop.Businness/Services/Implementations/CartItemService.cs
+++ Shop.Businness/Services/Implementations/CartItemService.cs
@@ -1,4 +1,8 @@
-﻿using Shop.Businness.Services.Interfaces;
+﻿using Shop.Businness.Responses;
+using Shop.Businness.Services.Interfaces;
+using Shop.Core.Utilities.Results.Abstract;
+using Shop.DTO.GetDTO;
+using Shop.DTO.PostDTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,24 @@ namespace Shop.Businness.Services.Implementations
 {
     public class CartItemService : ICartItemService
     {
+        public Task<IResult> CreateAsync(PostCartItemDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagginatedResponse<GetCartItemDTO>> GetAllAsync(int pageNumber = 1, int pageSize = 6)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<GetCartItemDTO>> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> UpdateAsync(int id, PostCartItemDTO dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Shop.Businness/Services/Interfaces/ICartItemService.cs
+++ Shop.Businness/Services/Interfaces/ICartItemService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Shop.Businness.Responses;
+using Shop.Core.Utilities.Results.Abstract;
+using Shop.DTO.GetDTO;
+using Shop.DTO.PostDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,9 @@ namespace Shop.Businness.Services.Interfaces
 {
     public interface ICartItemService
     {
-
+        public Task<PagginatedResponse<GetCartItemDTO>> GetAllAsync(int pageNumber = 1,int pageSize = 6);
+        public Task<IResult> CreateAsync(PostCartItemDTO dto);
+        public Task<IResult> UpdateAsync(int id, PostCartItemDTO dto);
+        public Task<IDataResult<GetCartItemDTO>> GetAsync(int id);
     }
 }
