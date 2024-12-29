b8d7edd84f926ce722b6e8502febc415b70a50a8
RasulSz22 <rasulsabziyev@gmail.com>
2024-12-29 12:24:21 +0400

    test

--- Shop.Businness/Registration/ServiceLayerServiceRegister.cs
+++ Shop.Businness/Registration/ServiceLayerServiceRegister.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Shop.Businness.Services.Implementations;
 using Shop.Businness.Services.Interfaces;
-using Shop.DataAccess.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shop.Businness.Registration
 {
