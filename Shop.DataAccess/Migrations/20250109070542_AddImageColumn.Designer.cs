25ab1e30f56b958bf746eaa79c8713b05a6ab72b
Rasul <Rasul@MOODGG>
2025-01-09 11:09:21 +0400

    SBC

--- Shop.DataAccess/Migrations/20250109070542_AddImageColumn.Designer.cs
+++ Shop.DataAccess/Migrations/20250109070542_AddImageColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shop.DataAccess.Contexts;
 
@@ -11,9 +12,11 @@ using Shop.DataAccess.Contexts;
 namespace Shop.DataAccess.Migrations
 {
     [DbContext(typeof(MyShoppingAPIDbContext))]
-    partial class MyShoppingAPIDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250109070542_AddImageColumn")]
+    partial class AddImageColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shop.DataAccess/Migrations/20250109070542_AddImageColumn.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_AppUserId1",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_AspNetUsers_AppUserId1",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Discounts_DiscountId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductImageFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_AppUserId1",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AppUserId1",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId1",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId1",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DiscountId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductImages",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AppUserId",
+                table: "Payments",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AppUserId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "Address",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_AppUserId",
+                table: "Payments",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AppUserId",
+                table: "Orders",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_AppUserId",
+                table: "Address",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_AspNetUsers_AppUserId",
+                table: "Address",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_AppUserId",
+                table: "Orders",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_AspNetUsers_AppUserId",
+                table: "Payments",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Discounts_DiscountId",
+                table: "Products",
+                column: "DiscountId",
+                principalTable: "Discounts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_AspNetUsers_AppUserId",
+                table: "Address");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_AppUserId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_AspNetUsers_AppUserId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Discounts_DiscountId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_AppUserId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AppUserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Address_AppUserId",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductImages",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Address");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DiscountId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId1",
+                table: "Payments",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId1",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ProductImageFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    AltText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FileType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsMain = table.Column<bool>(type: "bit", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImageFiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductImageFiles_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_AppUserId1",
+                table: "Payments",
+                column: "AppUserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AppUserId1",
+                table: "Orders",
+                column: "AppUserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImageFiles_ProductId",
+                table: "ProductImageFiles",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_AppUserId1",
+                table: "Orders",
+                column: "AppUserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_AspNetUsers_AppUserId1",
+                table: "Payments",
+                column: "AppUserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Discounts_DiscountId",
+                table: "Products",
+                column: "DiscountId",
+                principalTable: "Discounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
